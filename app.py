4787f90d7d0f91b9022e63b35122564cec9ce390
Arjun k <arjuarjun047@gmail.com>
2025-01-31 17:00:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("Secure File Upload & Access System")
 
 # Sidebar to choose action
 option = st.sidebar.radio("Choose an option:", ["Upload File", "Download or Delete File"])
-code=st.text_input("enter code")
+# code=st.text_input("enter code")
 if option == "Upload File":
     st.subheader("Upload a File")
     uploaded_file = st.file_uploader("Choose a file", type=None)
@@ -22,7 +22,7 @@ elif option == "Access a File":
     access_code = st.text_input("Enter access code to retrieve file")
 
     if st.button("Download File"):
-        file_name = file_handler.get_file_by_code(code)
+        file_name = file_handler.get_file_by_code(access_code )
         if file_name:
             file_path = os.path.join("uploads", file_name)
             st.download_button(label=f"Download {file_name}", data=open(file_path, "rb").read(), file_name=file_name)
