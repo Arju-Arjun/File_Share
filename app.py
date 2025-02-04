747d33b7e82306336b3c8e2a1a4ee30bf2820902
Arjun k <arjuarjun047@gmail.com>
2025-02-04 15:24:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ st.markdown(
             <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" width="30" height="30">
         </a>
         <a href="https://github.com/Arju-Arjun">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Logo_GitHub.png" width="30" height="30">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" width="30" height="30">
         </a>
         <a href="https://www.linkedin.com/in/arjun-k-525a45334/">
             <img src="https://upload.wikimedia.org/wikipedia/commons/7/76/LinkedIn_Logo_2023.png" width="30" height="30">
